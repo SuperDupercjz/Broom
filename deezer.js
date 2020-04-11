ab1938471eab1d18f6010ca0118a95092855b6ae
Steven Acevedo <astevenaa@gmail.com>
2020-04-11 16:20:04 -0700

    b

--- deezer.js
+++ deezer.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   var search = $("#Music").val();
-
+  var sweepSong = "";
   var timeElement = $(".currentTime");
   var secondsRemaining = 0;
 
@@ -13,7 +13,10 @@ $(document).ready(function () {
 
   $("#musicButton").on("click", function (event) {
     event.preventDefault();
-    $("#divC").empty();
+    if ($("#divC").children().length > 0) {
+      $("#divC").slick("unslick");
+      $("#divC").empty();
+    }
     search = $("#Music").val();
     musicGrab();
 
@@ -22,27 +25,45 @@ $(document).ready(function () {
   });
   $(".toggle-play").on("click", function () {
     song.trigger("play");
-	isPlaying = true;
-	var timeIntervalId = setInterval(function(){
-		secondsRemaining++;
-		timeElement.text("00:0" + secondsRemaining)
-		$(".time").val(secondsRemaining);
-		if (secondsRemaining === 7){
-			song.trigger("pause");
-			isPlaying=false;
-			clearInterval(timeIntervalId)
-			secondsRemaining = 0;
-		  }
-	}, 1000);
+    isPlaying = true;
+    var timeIntervalId = setInterval(function () {
+      secondsRemaining++;
+      timeElement.text("00:0" + secondsRemaining);
+      $(".time").val(secondsRemaining);
+      if (secondsRemaining === 7) {
+        song.trigger("pause");
+        isPlaying = false;
+        clearInterval(timeIntervalId);
+        secondsRemaining = 0;
+      }
+    }, 1000);
 
     console.log("playing...");
   });
 
-  $(".toggle-pause").on("click", function () {
-    song.trigger("pause");
-    isPlaying = false;
-    console.log("paused...");
+  $("#sweep").on("click", function (event) {
+    event.preventDefault();
+    $(".div1").empty();
+    var sweepPlay = $("<button>");
+    sweepPlay.attr("id", "sweepPlay");
+    sweepPlay.text("Play");
+    $(".div1").append(sweepPlay);
+    $(".div1").append(song);
+
+    song.attr("src", sweepSong);
+
+    $("#sweepPlay").on("click", function (event) {
+      event.preventDefault();
+      song.trigger("play");
+      isPlaying = true;
+    });
   });
+
+  //   $(".toggle-pause").on("click", function () {
+  //     song.trigger("pause");
+  //     isPlaying = false;
+  //     console.log("paused...");
+  //   });
 
   // $("#divC").on("click", function (event) {
   // 	// audioPlayer.get(0).play()
@@ -70,10 +91,12 @@ $(document).ready(function () {
       console.log(response);
       var musicData = response.data;
       for (let i = 0; i < musicData.length; i++) {
+        console.log(response);
         var newTitle = $("<h4>");
         var newDiv = $("<div>");
         newDiv.attr("class", "has-text-centered");
         song.attr("src", musicData[i].preview);
+        newTitle.attr("class", "has-text-white");
         // audioPlayer.attr("class", "play")
         var newImg = $("<img>");
         newTitle.text(musicData[i].title_short);
@@ -87,6 +110,8 @@ $(document).ready(function () {
 
       $("img").on("click", function (event) {
         song.attr("src", event.target.name);
+        var selectedSong = event.target.name;
+        sweepSong = selectedSong;
         console.log(event.target.name);
       });
 
